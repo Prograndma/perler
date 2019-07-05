0edee821e05f5fbf27c435402066f69d74563ddd
Prograndma <thomasvcole@gmail.com>
2019-07-04 23:29:00 -0600

    The pictures show an old bug.
    :::::::NEW STUFF:::::::::
    The code is cleaned up a little bit. I still wonder if I
    could make the pixel_scatter function prettier. It's pretty
    big right now.
    Added a pixel_scatter example image.
    
    :::::::past problems fixed:::::::
    
    In the current code there are (blue, green, red) _count
    variables. In the previous code it was just one variable
    count. This variable was intended to be used in the
    update_pixel_count function to update which row the pixel was
    taken from. (You want to take the pixel from one row three
    times to get the proper picture). Count would reset each
    time it hit nine, and row would increase. The problem was
    that count would only hit nine during one color, so only one
    color would change its row. The other colors would be stuck
    on the same row forever. The fix was simple, and easy.
    Use three different variables, duhhh. This has been working.
    
    :::::::::FUTURE DEVELOPMENT::::::
    -find the colors of perlers available, create a palette
     out of them.
    
    -implement a dithering function with the perler.

--- picture_maker.py
+++ picture_maker.py
@@ -1,15 +1,56 @@
 import cv2 as cv
 import numpy as np
 import imutils
-from copy import deepcopy as copy
-from random import randint
+
+
 size = 250
-slash_num = 255 / 10
+
+
+def split_image_into_bgr_grayscale(image):
+    image_b = np.array(image)[:, :, 0]
+    image_g = np.array(image)[:, :, 1]
+    image_r = np.array(image)[:, :, 2]
+    return image_b, image_g, image_r
+
+
+def split_image_into_bgr_color(image):
+    b_scale = np.asarray([[[0 for pixel in range(3)] for row in range(size)] for column in range(size)], dtype=np.uint8)
+    g_scale = np.asarray([[[0 for pixel in range(3)] for row in range(size)] for column in range(size)], dtype=np.uint8)
+    r_scale = np.asarray([[[0 for pixel in range(3)] for row in range(size)] for column in range(size)], dtype=np.uint8)
+
+    b_row_counter = 0
+    b_column = 0
+    g_row_counter = 0
+    g_column = 0
+    r_row_counter = 0
+    r_column = 0
+
+    blue_i, green_i, red_i = split_image_into_bgr_grayscale(image)
+
+    for b_row, g_row, r_row in zip(b_scale, g_scale, r_scale):
+        for b_pix, g_pix, r_pix in zip(b_row, g_row, r_row):
+            blue, b_row, b_column = update_pixel(blue_i, b_row_counter, b_column)
+            green, g_row_counter, g_column = update_pixel(green_i, g_row_counter, g_column)
+            red, r_row_counter, r_column = update_pixel(red_i, r_row_counter, r_column)
+
+            b_pix += np.asarray([blue, 0, 0], dtype=np.uint8)
+            g_pix += np.asarray([0, green, 0], dtype=np.uint8)
+            r_pix += np.asarray([0, 0, red], dtype=np.uint8)
+
+
+def update_pixel(image, row, column):
+    pix = image[row][column]
+    column += 1
+    if column == size:
+        column = 0
+        row += 1
+        if row == size:
+            row = 0
+    return pix, row, column
 
 
 def update_pixel_count(image, row, column, count):
     pix = image[row][column]
-    average = np.average([image_not[row][column], image_one[row][column], image_two[row][column]])
     column += 1
     if column == size:
         column = 0
@@ -19,134 +60,100 @@ def update_pixel_count(image, row, column, count):
             row += 1
             if row == size:
                 row = 0
-    return pix, row, column, count, average
+    return pix, row, column, count
 
 
-def update_pixel(image, row, column):
-    pix = image[row][column]
-    column += 1
-    if column == size:
-        column -= size
-        row += 1
-        if row == size:
-            row -= size
-    return pix, row, column
+def slash(num_shades, image):
+    if num_shades < 1:
+        divisor = 255 / num_shades - 1
+        result = np.uint8(image / divisor)
+        result = np.uint8(result * divisor)
+    else:
+        print("can't make so few colors")
+        return None
+    return result
+
+
+def pixel_scatter(image, destination=None):
+    if destination is None:
+        temp = [[[0 for pixel in range(3)] for row in range(size * 3)] for col in range(size * 3)]
+        destination = np.asarray(temp, dtype=np.uint8)
+
+    image_blue, image_green, image_red = split_image_into_bgr_grayscale(image)
+
+    blue_row = 0
+    blue_column = 0
+    blue_count = 0
+
+    green_row = 0
+    green_column = 0
+    green_count = 0
+
+    red_row = 0
+    red_column = 0
+    red_count = 0
+
+    for i, row in enumerate(destination):
+        for j, pixel in enumerate(row):
+            if i % 3 == 0:
+                if j % 3 == 0:
+                    blue_pixel, blue_row, blue_column, blue_count = update_pixel_count(image_blue, blue_row,
+                                                                                       blue_column, blue_count)
+                    pixel += np.asarray([blue_pixel, 0, 0], dtype=np.uint8)
+                elif j % 3 == 1:
+                    green_pixel, green_row, green_column, green_count = update_pixel_count(image_green, green_row,
+                                                                                           green_column, green_count)
+                    pixel += np.asarray([0, green_pixel, 0], dtype=np.uint8)
+                else:
+                    red_pixel, red_row, red_column, red_count = update_pixel_count(image_red, red_row, red_column,
+                                                                                   red_count)
+                    pixel += np.asarray([0, 0, red_pixel], dtype=np.uint8)
+            elif i % 3 == 1:
+                if j % 3 == 0:
+                    red_pixel, red_row, red_column, red_count = update_pixel_count(image_red, red_row, red_column,
+                                                                                   red_count)
+                    pixel += np.asarray([0, 0, red_pixel], dtype=np.uint8)
+                elif j % 3 == 1:
+                    blue_pixel, blue_row, blue_column, blue_count = update_pixel_count(image_blue, blue_row,
+                                                                                       blue_column, blue_count)
+                    pixel += np.asarray([blue_pixel, 0, 0], dtype=np.uint8)
+                else:
+                    green_pixel, green_row, green_column, green_count = update_pixel_count(image_green, green_row,
+                                                                                           green_column, green_count)
+                    pixel += np.asarray([0, green_pixel, 0], dtype=np.uint8)
+            else:
+                if j % 3 == 0:
+                    green_pixel, green_row, green_column, green_count = update_pixel_count(image_green, green_row,
+                                                                                           green_column, green_count)
+                    pixel += np.asarray([0, green_pixel, 0], dtype=np.uint8)
+                elif j % 3 == 1:
+                    red_pixel, red_row, red_column, red_count = update_pixel_count(image_red, red_row, red_column,
+                                                                                   red_count)
+                    pixel += np.asarray([0, 0, red_pixel], dtype=np.uint8)
+                else:
+                    blue_pixel, blue_row, blue_column, blue_count = update_pixel_count(image_blue, blue_row,
+                                                                                       blue_column, blue_count)
+                    pixel += np.asarray([blue_pixel, 0, 0], dtype=np.uint8)
+
+    return destination
 
 
 if __name__ == "__main__":
     path = 'C:\\Users\\Thomas\\Pictures\\Saved Pictures\\thomas_cropped.jpg'
-    image = cv.imread(path)                                                 # BGR
-    image = imutils.resize(image, width=size, height=size)
-    lst = [[[0 for col in range(3)] for col in range(size * 3)] for row in range(size * 3)]
-    perler = np.asarray(lst, dtype=np.uint8)
-    slash = np.asarray(copy(image), dtype=np.uint8)
+    save_path = 'C:\\Users\\Thomas\\Pictures\\Saved Pictures\\'
+    image_original = cv.imread(path)                                                 # BGR
+    if image_original is None:
+        print("Could not load image from path:")
+        print(path)
+        exit()
 
-    image_not = np.array(copy(image))[:, :, 0]
-    image_one = np.array(copy(image))[:, :, 1]
-    image_two = np.array(copy(image))[:, :, 2]
-
-    to_use = 'not'
-    not_row = 0
-    not_column = 0
-    one_row = 0
-    one_column = 0
-    two_row = 0
-    two_column = 0
-    naught_count = 0
-    one_count = 0
-    two_count = 0
-    average = 0
-    for i, row in enumerate(perler):
-        for j, pixel in enumerate(row):
-            if i % 3 == 0:
-                if to_use == 'not':
-                    naught, not_row, not_column, naught_count, average = update_pixel_count(image_not, not_row, not_column, naught_count)
-                    pixel += np.asarray([naught, 0, 0], dtype=np.uint8)
-                    to_use = 'one'
-                elif to_use == 'one':
-                    one, one_row, one_column, one_count, average = update_pixel_count(image_one, one_row, one_column, one_count)
-                    pixel += np.asarray([0, one, 0], dtype=np.uint8)
-                    to_use = 'two'
-                else:
-                    two, two_row, two_column, two_count, average = update_pixel_count(image_two, two_row, two_column, two_count)
-                    pixel += np.asarray([0, 0, two], dtype=np.uint8)
-                    to_use = 'not'
-            elif i % 3 == 1:
-                if to_use == 'not':
-                    two, two_row, two_column, two_count, average = update_pixel_count(image_two, two_row, two_column, two_count)
-                    pixel += np.asarray([0, 0, two], dtype=np.uint8)
-                    to_use = 'one'
-                elif to_use == 'one':
-                    naught, not_row, not_column, naught_count, average = update_pixel_count(image_not, not_row, not_column, naught_count)
-                    pixel += np.asarray([naught, 0, 0], dtype=np.uint8)
-                    to_use = 'two'
-                else:
-                    one, one_row, one_column, one_count, average = update_pixel_count(image_one, one_row, one_column, one_count)
-                    pixel += np.asarray([0, one, 0], dtype=np.uint8)
-                    to_use = 'not'
-            else:
-                if to_use == 'not':
-                    one, one_row, one_column, one_count, average = update_pixel_count(image_one, one_row, one_column, one_count)
-                    pixel += np.asarray([0, one, 0], dtype=np.uint8)
-                    to_use = 'one'
-                elif to_use == 'one':
-                    two, two_row, two_column, two_count, average = update_pixel_count(image_two, two_row, two_column, two_count)
-                    pixel += np.asarray([0, 0, two], dtype=np.uint8)
-                    to_use = 'two'
-                else:
-                    naught, not_row, not_column, naught_count, average = update_pixel_count(image_not, not_row, not_column, naught_count)
-                    pixel += np.asarray([naught, 0, 0], dtype=np.uint8)
-                    to_use = 'not'
-
-    blue_scale = np.asarray([[[0 for col in range(3)] for col in range(size)] for row in range(size)], dtype=np.uint8)
-    green_scale = np.asarray([[[0 for col in range(3)] for col in range(size)] for row in range(size)], dtype=np.uint8)
-    red_scale = np.asarray([[[0 for col in range(3)] for col in range(size)] for row in range(size)], dtype=np.uint8)
-
-    not_row = 0
-    not_column = 0
-    one_row = 0
-    one_column = 0
-    two_row = 0
-    two_column = 0
-
-    for blue_row, green_row, red_row in zip(blue_scale, green_scale, red_scale):
-        for b_pix, g_pix, r_pix in zip(blue_row, green_row, red_row):
-            naught, not_row, not_column = update_pixel(image_not, not_row, not_column)
-            one, one_row, one_column = update_pixel(image_one, one_row, one_column)
-            two, two_row, two_column = update_pixel(image_two, two_row, two_column)
-
-            b_pix += np.asarray([naught, 0, 0], dtype=np.uint8)
-            g_pix += np.asarray([0, one, 0], dtype=np.uint8)
-            r_pix += np.asarray([0, 0, two], dtype=np.uint8)
-
-    slash = np.uint8(slash / slash_num)
-    slash = np.uint8(slash * slash_num)
-
-    # perler = imutils.resize(perler, width=size, height=size)
-
-    cv.imshow('blue_scale', blue_scale)
-    cv.imshow('green_scale', green_scale)
-    cv.imshow('red_scale', red_scale)
-
-    cv.imshow('not', image_not)
-    cv.imshow('one', image_one)
-    cv.imshow('two', image_two)
+    image_original = imutils.resize(image_original, width=size, height=size)
+    perler = pixel_scatter(image_original)
 
     cv.imshow('pixels', perler)
-    cv.imshow('original', image)
-    cv.imshow('slash', slash)
+    cv.imshow('original', image_original)
 
-    cv.imshow('blow_up', imutils.resize(perler, width=size * 12))
-    # if not cv.imwrite(path + 'blue.png', imutils.resize(perler, width=size * 12)):
-    #     print("couldn't save")
-
-    weird_blue = cv.imread(path + 'blue.png')
-    weird_green = cv.imread(path + 'green.png')
-    weird_red = cv.imread(path + 'red.png')
-
-    ave_weird = np.average([weird_blue, weird_red, weird_green], axis=0)
-
-    cv.imshow('ave_weird', ave_weird)
-    # if not cv.imwrite(path + 'ave.png', ave_weird):
-    #     print("couldn't save")
     cv.waitKey(0)
+
+
+# ave_weird = np.average([weird_blue, weird_red, weird_green], axis=0)
